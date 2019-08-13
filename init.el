2ec3809c9fd8f5a0cc12af593230a1eb89885cf8
Jeff McCarrell <jeff@mccarrell.org>
2019-08-12 21:33:47 -0700

    javascript and json

--- init.el
+++ init.el
@@ -336,6 +336,21 @@
            (string= lang "sqlite"))))
 (setq org-confirm-babel-evaluate 'my-org-confirm-babel-evaluate)
 
+(use-package js2-mode
+  :init
+  (setq js-basic-indent 2)
+  (setq-default js2-basic-indent 2
+                js2-basic-offset 2
+                js2-auto-indent-p t
+                js2-cleanup-whitespace t
+                js2-enter-indents-newline t
+                js2-indent-on-enter-key t
+                js2-global-externs (list "window" "module" "require" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname" "console" "JSON" "jQuery" "$"))
+  (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+  (add-to-list 'auto-mode-alist '("\\.es6$" . js2-mode)))
+
+(use-package json-mode)
+
 ;;; Post initialization
 
 (let ((elapsed (float-time (time-subtract (current-time)
