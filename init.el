fd70ad2e8f1f0499f360436351db338ac611f6de
Jeff McCarrell <jeff@mccarrell.org>
2019-08-30 17:36:36 -0700

    add expand-region.

--- init.el
+++ init.el
@@ -153,6 +153,37 @@
   (set-face-attribute 'whitespace-indentation nil :foreground "#666666" :background nil)
   :diminish whitespace-mode)
 
+(use-package expand-region
+  :ensure t
+  :config
+  (defun ha/expand-region (lines)
+    "Prefix-oriented wrapper around Magnar's `er/expand-region'.
+
+     Call with LINES equal to 1 (given no prefix), it expands the
+     region as normal.  When LINES given a positive number, selects
+     the current line and number of lines specified.  When LINES is a
+     negative number, selects the current line and the previous lines
+     specified.  Select the current line if the LINES prefix is zero."
+    (interactive "p")
+    (cond ((= lines 1)   (er/expand-region 1))
+          ((< lines 0)   (ha/expand-previous-line-as-region lines))
+          (t             (ha/expand-next-line-as-region (1+ lines)))))
+
+  (defun ha/expand-next-line-as-region (lines)
+    (message "lines = %d" lines)
+    (beginning-of-line)
+    (set-mark (point))
+    (end-of-line lines))
+
+  (defun ha/expand-previous-line-as-region (lines)
+    (end-of-line)
+    (set-mark (point))
+    (beginning-of-line (1+ lines)))
+
+  ;; jwm: however, I can't seem to get C-= from my mac keyboard.
+  ;;   so prefer C-@
+  :bind ("C-@" . ha/expand-region))
+
 (use-package ace-window
   :bind (("M-o" . ace-window))
   :config
