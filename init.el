42639f797d6fdd51651a5bd5789b38f4022b4f72
Jeff McCarrell <jeff@mccarrell.org>
2023-09-11 12:35:16 -0700

    drop unused docker packages.

--- init.el
+++ init.el
@@ -455,13 +455,6 @@
   :bind ("C-c d" . docker)
   :diminish)
 
-(use-package docker-compose-mode
-  :mode "docker-compose.*\.yml\\'")
-
-(use-package docker-tramp
-  :after tramp
-  :defer 5)
-
 (use-package dockerfile-mode
   :mode "Dockerfile[a-zA-Z.-]*\\'")
 
